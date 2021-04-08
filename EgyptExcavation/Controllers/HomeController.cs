4e8af5c114a516e1de9bb1e50f6c519b49fe1e53
Kinzie Hamilton <61798938+mackinz2@users.noreply.github.com>
2021-04-08 16:31:58 -0600

    EditFieldNotes and RemoveFieldNotes Added

--- EgyptExcavation/Controllers/HomeController.cs
+++ EgyptExcavation/Controllers/HomeController.cs
@@ -13,13 +13,16 @@ namespace EgyptExcavation.Controllers
 {
     public class HomeController : Controller
     {
+        //Initalize and Bring in Context
+        private egyptexcavationContext context { get; set; }
+
         private readonly ILogger<HomeController> _logger;
         //private egyptexcavationContext context { get; set; } , egyptexcavationContext con
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, egyptexcavationContext con)
         {
             _logger = logger;
-            //context = con;
+            context = con;
         }
 
         public IActionResult Index()
@@ -37,9 +40,46 @@ namespace EgyptExcavation.Controllers
             return View();
         }
 
-        public IActionResult EditFieldNotes()
+
+        [HttpPost]
+        public IActionResult EditFieldNotes1(int BurialID)
         {
-            return View();
+            Burial b = context.Burial.Single(x => x.BurialId == BurialID);
+            return View("EditFieldNotes", b);
+
+        }
+
+        [HttpPost]
+        public IActionResult EditFieldNotes2(Burial b, int BurialID)
+        {
+            if (ModelState.IsValid)
+            {
+                var bur = context.Burial.SingleOrDefault(x => x.BurialId == b.BurialId);
+
+                context.Entry(bur).Property(x => x.quoteText).CurrentValue = q.quoteText;
+                context.Entry(bur).Property(x => x.Author).CurrentValue = q.Author;
+                context.Entry(bur).Property(x => x.quoteDate).CurrentValue = q.quoteDate;
+                context.Entry(bur).Property(x => x.Subject).CurrentValue = q.Subject;
+                context.Entry(bur).Property(x => x.Citation).CurrentValue = q.Citation;
+
+
+                context.SaveChanges();
+
+                return RedirectToAction("BurialList");
+            }
+            else
+                return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Remove(int BurialID)
+        {
+            Burial b = context.Burial.Single(x => x.BurialId == BurialID);
+            context.Remove(b);
+            context.SaveChanges();
+            return RedirectToAction("BurialList");
         }
 
         public IActionResult EditMummyInfo()
